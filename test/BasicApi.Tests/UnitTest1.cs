59ea4bbf04927b278d8e6bdff2c65dec2c843cd6
Vishnu Vishweshwar <VISH396534@exactsoftware.com>
2024-03-07 10:56:46 +0530

    Initial commit

---
+++ test/BasicApi.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using BasicApi.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace BasicApi.Tests
+{
+    public class Tests
+    {
+        WeatherForecastController weatherForecastController;
+        Mock<ILogger<WeatherForecastController>> loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        [SetUp]
+        public void Setup()
+        {
+            weatherForecastController = new WeatherForecastController(loggerMock.Object);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var results = weatherForecastController.Get();
+
+            Assert.That(results.Count, Is.EqualTo(10));
+        }
+    }
+}
