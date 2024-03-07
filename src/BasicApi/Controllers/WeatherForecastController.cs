c004a4b50c4ccc72168a816173bf9e79a2c60f3a
Vishnu Vishweshwar <VISH396534@exactsoftware.com>
2024-03-07 12:17:17 +0530

    s

--- src/BasicApi/Controllers/WeatherForecastController.cs
+++ src/BasicApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace BasicApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchinggg"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
